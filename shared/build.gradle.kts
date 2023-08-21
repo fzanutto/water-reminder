19262fba3ec2885b36536fb6f1324d5e4facd27d
Fernando Zanutto <ferzanutto1999@gmail.com>
2023-08-20 22:07:20 -0300

    update dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -46,9 +46,8 @@ kotlin {
         }
         val androidMain by getting {
             dependencies {
-                api("androidx.activity:activity-compose:1.6.1")
-                api("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
-                api("androidx.core:core-ktx:1.9.0")
+                api("androidx.activity:activity-compose:1.7.2")
+                api("androidx.core:core-ktx:1.10.1")
                 api(compose.preview)
                 api(compose.uiTooling)
             }
